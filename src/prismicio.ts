48dfeb2a532589222f87cc94d704a7b72eb22eb5
dipalab <oska.aditya18@gmail.com>
2026-01-15 12:38:38 +0700

    fix: config prismic

--- src/prismicio.ts
+++ src/prismicio.ts
@@ -33,10 +33,7 @@ const routes: Route[] = [
 export const createClient = (config: ClientConfig = {}) => {
   const client = baseCreateClient(repositoryName, {
     routes,
-    fetchOptions:
-      process.env.NODE_ENV === "production"
-        ? { next: { tags: ["prismic"] }, cache: "force-cache" }
-        : { next: { revalidate: 5 } },
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN, 
     ...config,
   });
 
